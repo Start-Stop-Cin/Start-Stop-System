7b1788367907e1860170088837413aacd6de32a0
Mateus-P-Lourenco <mateuslourenco273@gmail.com>
2026-04-02 15:08:59 -0300

    Created the 3 main functions of the AutoStop Inhibit functionality

--- include/inhibit_safety_conditions.h
+++ include/inhibit_safety_conditions.h
@@ -3,4 +3,8 @@
 
 int inhibit_safety_conditions( float InclinationAngle, int DoorStatus, int SeatbeltStatus, int GearPosition, int SS_Enabled );
 
+int standstill_management( int AutoStopActive, int BrakeStatus, int DoorStatus, int SeatbeltStatus );
+
+int sr_flip_flop(int S, int R);
+
 #endif
