ab4e1d2f7a361ca62443695285ab2c355fe2aa17
Mateus-P-Lourenco <mateuslourenco273@gmail.com>
2026-04-02 14:20:08 -0300

    Added the inhibit_safety_condition function

---
+++ include/inhibit_safety_conditions.h
@@ -0,0 +1,6 @@
+#ifndef INHIBIT_SAFETY_CONDITIONS_H
+#define INHIBIT_SAFETY_CONDITIONS_H
+
+int inhibit_safety_conditions( float InclinationAngle, int DoorStatus, int SeatbeltStatus, int GearPosition, int SS_Enabled );
+
+#endif
