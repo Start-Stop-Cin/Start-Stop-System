a54aec0cc9ae1c6df209c97bd0907f551a03c1f5
bhsa <bhsalmeida16@gmail.com>
2026-04-10 23:09:27 -0300

    chore: includes linux_build

---
+++ src/Cluster_SIL/linux_build/CMakeFiles/ClusterSIL_qmllint_json.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClusterSIL_qmllint_json"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ClusterSIL_qmllint_json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Cluster_SIL/linux_build/CMakeFiles/ClusterSIL_qmltyperegistration.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClusterSIL_qmltyperegistration"
+  "ClusterUI/ClusterSIL.qmltypes"
+  "clustersil_qmltyperegistrations.cpp"
+  "meta_types/qt6clustersil_release_metatypes.json"
+  "meta_types/qt6clustersil_release_metatypes.json.gen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ClusterSIL_qmltyperegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
